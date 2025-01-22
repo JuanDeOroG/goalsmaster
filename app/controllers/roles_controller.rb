e5827f3369f6d28b8d7bb0ae2f7f0f6a6a94491d
Juan D. De Oro <juandeoro4@gmail.com>
2025-01-22 17:14:32 -0500

    add role destroy and modularization

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,20 +1,56 @@
 require_dependency "Roles/ListRolesUseCase"
+
 class RolesController < ApplicationController
-    def initialize
-      @listRolesUseCase = ListRolesUseCase.new
-      super()
-    end
-    def index
-        @roles = @listRolesUseCase.call(params)
-    end
+  before_action :authenticate_user!
 
-    def create
-      role = Role.new(name: params[:name])
-      if role.save
-        redirect_to roles_path, notice: "Role was create successfully"
-      else
-        redirect_to roles_path, alert: "Role was not create"
+  def initialize
+    @listRolesUseCase = ListRolesUseCase.new
+    super()
+  end
 
-      end
+  def index
+    @roles = @listRolesUseCase.call(params, true)
+    @role = Role.new # Para el formulario de creación
+  end
+
+  def create
+    role = Role.new(role_params)
+    if role.save
+      redirect_to roles_path, notice: "Role was created successfully"
+    else
+      error_messages = role.errors.full_messages.join(", ")
+      redirect_to roles_path, alert: "Role was not Created: #{error_messages}"
     end
+  end
+
+  def edit
+    @role = Role.find(params[:id])
+    render partial: "roles/partials/form", locals: { role: @role }
+  end
+
+  def update
+    role = Role.find(params[:id])
+    if role.update(role_params)
+      redirect_to roles_path, notice: "Role was updated successfully"
+    else
+      error_messages = role.errors.full_messages.join(", ")
+      redirect_to roles_path, alert: "Role was not updated: #{error_messages}"
+    end
+  end
+
+  def destroy
+    role = Role.find(params[:id])
+    if role.update(state: 2)
+      redirect_to roles_path, notice: "Role was deleted successfully"
+    else
+      error_messages = role.errors.full_messages.join(", ")
+      redirect_to roles_path, alert: "Role was not Deleted: #{error_messages}"
+    end
+  end
+
+  private
+
+  def role_params
+    params.require(:role).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :parameters, only: [ :index, :show, :new, :create, :edit, :update ]
   resources :parameter_values, only: [ :index, :show, :new, :create, :edit, :update ]
 
-  resources :roles, only: [ :index, :show, :new, :create, :edit, :update ]
+  resources :roles, only: [ :index, :show, :new, :create, :edit, :update, :destroy ]
 end
