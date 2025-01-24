cb1233e5bfa7a7de9b11edee33cda620571ac487
Juan D. De Oro <juandeoro4@gmail.com>
2025-01-23 21:34:35 -0500

    permissions: edit and destroy

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -23,6 +23,33 @@ class PermissionsController < ApplicationController
     end
   end
 
+  def edit
+    @permission = Permission.find(params[:id])
+    render partial: "permissions/partials/form", locals: { permission: @permission }
+  end
+
+  def update
+    permission = Permission.find(params[:id])
+
+    if permission.update(permission_params)
+      redirect_to permissions_path, notice: "Permission updated successfully"
+    else
+      error_messages = permission.errors.full_messages.join(", ")
+      redirect_to permissions_path, alert: "Permission was not Updated: #{error_messages}"
+    end
+  end
+
+  def destroy
+    permission = Permission.find(params[:id])
+    # Change state to deleted (2)
+    if permission.update(state: 2)
+      redirect_to permissions_path, notice: "Permission deleted successfully"
+    else
+      error_messages = permission.errors.full_messages.join(", ")
+      redirect_to permissions_path, alert: "Permission was not deleted: #{error_messages}"
+    end
+  end
+
   private
 
   def permission_params
